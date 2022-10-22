2bcaf10281d7597d4de2ffee9d61f716151417b5
Victor E B Barros <victoremanuel6778@gmail.com>
2022-10-22 15:20:33 -0300

    UltChanges

---
+++ RainDark_/Assets/Mecanica/Canvas/GameController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public int life;
+    public Text  txtLife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+   public void Dano(){
+        life--;
+        txtLife.text = "Life: " + life;
+    }
+
+    public void DanoUp(){
+        life-= 3;
+        txtLife.text = "Life: " + life;
+    }
+
+    public void HitKill(){
+        life-=6;
+        txtLife.text = "Life: " + life;
+    }
+
+}
